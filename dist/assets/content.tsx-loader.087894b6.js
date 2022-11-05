8c5ba15c60c4c53fa367d86f14f896960b00959c
Jack Allen <jackallensemail+git@gmail.com>
2022-11-05 23:08:48 +0000

    stuff

--- dist/assets/content.tsx-loader.087894b6.js
+++ dist/assets/content.tsx-loader.087894b6.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/content.tsx.0f5262c0.js")
+      chrome.runtime.getURL("assets/content.tsx.b449b2e8.js")
     );
   })().catch(console.error);
 
